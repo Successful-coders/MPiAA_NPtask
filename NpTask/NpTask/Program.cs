86e98e15afb5dd8f67d8c7bbb66de32af2d7b0fe
Voronik <voronkina.2018@azure.nstu.ru>
2019-11-21 22:39:59 +0700

    all work

--- NpTask/NpTask/Program.cs
+++ NpTask/NpTask/Program.cs
@@ -5,24 +5,26 @@ using System.IO;
 
 namespace NpTask
 {
-    class Automobile
+    public class Automobile
     {
         public int price = 0;
         public int weight = 0;
+        public int koef = 0;
         public Automobile(int _weight, int _price)
         {
             price = _price;
             weight = _weight;
+            koef = price / weight;
         }
     }
  
 
-    class Parom
+    public class Parom
     {
         public List<Automobile> subset = new List<Automobile>();//множество машин
-        List<Automobile> result = null;
+        public List<Automobile> result = new List<Automobile>();
         private int maxWeight = 0;
-        private int bestPrice = 0;
+        public int bestPrice = 0;
         public int numberOfAutomobile = 0;
         public Parom(int maxWeight_)
         {
@@ -35,18 +37,18 @@ namespace NpTask
             string writeLine = "";
             Random rnd = new Random();
             StreamWriter sw;
-            FileInfo fi = new FileInfo(filename); // информация о файле 
-            sw = fi.CreateText(); // или поток для записи 
+            FileInfo fi = new FileInfo(filename);
+            sw = fi.CreateText();
 
-            int number = 13;//rnd.Next(2, 15);
+            int number = rnd.Next(2, 15);
             numberOfAutomobile = number;
             sw.WriteLine(number.ToString());
             for (int i = 0; i < number; i++)
             {
-                int weight = (rnd.Next(1, 100));
+                int weight = (rnd.Next(1, 10));
                 int price = (rnd.Next(1, 100));
 
-                writeLine = weight.ToString() + "-" + price.ToString() ;
+                writeLine = weight.ToString() + "-" + price.ToString();
                 sw.WriteLine(writeLine);
 
                 subset.Add(new Automobile(weight, price));
@@ -55,36 +57,42 @@ namespace NpTask
 
         }
 
-        
-        private int SummW(List<Automobile> automobileSet)
+
+        public int SummW(List<Automobile> automobileSet)
         {
             int sumW = 0;
-
-            for (int i = 0; i < automobileSet.Count; i++)
+            if (automobileSet != null)
             {
 
-                sumW += automobileSet[i].weight;
-            }
-            
+                for (int i = 0; i < automobileSet.Count; i++)
+                {
 
+                    sumW += automobileSet[i].weight;
+                }
+
+
+            }
             return sumW;
         }
 
-        private int SummP(List<Automobile> automobileSet)
+        public int SummP(List<Automobile> automobileSet)
         {
             int sumPrice = 0;
-
-            for (int i = 0; i < automobileSet.Count; i++)
+            if (automobileSet != null)
             {
 
-                sumPrice += automobileSet[i].price;
+                for (int i = 0; i < automobileSet.Count; i++)
+                {
+
+                    sumPrice += automobileSet[i].price;
+                }
+
+
             }
-
-
             return sumPrice;
         }
 
-        private void CheckSet(List<Automobile> automobileSet)
+        public void CheckSet(List<Automobile> automobileSet)
         {
             if (result == null)
             {
@@ -105,29 +113,93 @@ namespace NpTask
         }
         public void EnumerationAutomobile (List<Automobile> automobileSet)
         {
-            if (automobileSet.Count > 0)
-                CheckSet(automobileSet);
-
-            for (int i = 0; i < automobileSet.Count; i++)
+            if (automobileSet != null)
             {
-                List<Automobile> newSet = new List<Automobile>(automobileSet);
+                if (automobileSet.Count > 0)
+                    CheckSet(automobileSet);
 
-                newSet.RemoveAt(i);
+                for (int i = 0; i < automobileSet.Count; i++)
+                {
+                    List<Automobile> newSet = new List<Automobile>(automobileSet);
 
-                EnumerationAutomobile(newSet);
+                    newSet.RemoveAt(i);
+
+                    EnumerationAutomobile(newSet);
+                }
             }
-
         }
 
-        public void 
-      
+        private int CompareKoef(int x, int y)
+        {
+            if (x != y)
+            {
+                if (x > y)
+                    return 1;
+                if (x < y)
+                    return -1;
+            }
+            return 0;
+        }
+        private void SortKoef ()
+        {
+            if (subset != null)
+            {
+                int temp;
+                for (int i = 0; i < subset.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < subset.Count; j++)
+                    {
+                        if (CompareKoef(subset[i].koef, subset[j].koef) == -1)
+                        {
+                            temp = subset[i].koef;
+                            subset[i].koef = subset[j].koef;
+                            subset[j].koef = temp;
+
+                            temp = subset[i].price;
+                            subset[i].price = subset[j].price;
+                            subset[j].price = temp;
+
+                            temp = subset[i].weight;
+                            subset[i].weight = subset[j].weight;
+                            subset[j].weight = temp;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public void GreedyAlgorithm()
+        {
+            if (subset != null)
+            {
+                SortKoef();
+                if ((result.Count == 0) && (subset[0].weight <= maxWeight))
+                {
+                    result.Add(new Automobile(subset[0].weight, subset[0].price));
+                    bestPrice += subset[0].price;
+                }
+                for (int i = 1; i < subset.Count; i++)
+                {
+                    List<Automobile> newSet = new List<Automobile>(result);
+                    newSet.Add(new Automobile(subset[i].weight, subset[i].price));
+                    if (SummW(newSet) <= maxWeight)
+                    {
+                        result.Add(new Automobile(subset[i].weight, subset[i].price));
+                        bestPrice += subset[i].price;
+                    }
+                    newSet.Clear();
+                }
+            }
+        }
+
     }
     class Program
     {
         static void Main()
         {
             Random rnd = new Random();
-            int MAX = rnd.Next(10, 400);
+            int MAX =  rnd.Next(10, 120);
             Parom parom = new Parom(MAX);
 
             parom.Create("input.txt");
@@ -143,6 +215,18 @@ namespace NpTask
                 ts.Hours, ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            parom.GreedyAlgorithm();
+            stopwatch2.Stop();
+            TimeSpan ts2 = stopwatch2.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts2.Hours, ts2.Minutes, ts2.Seconds,
+                ts2.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime2);
         }
     }
 }
--- NpTask/XUnitTestProject1/UnitTest1.cs
+++ NpTask/XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,150 @@
 using System;
+using NpTask;
 using Xunit;
-
 namespace XUnitTestProject1
 {
-    public class UnitTest1
+    public class GredyAlgTest
     {
         [Fact]
-        public void Test1()
+        public void GreedyAlgorithm_Empty()
         {
+            Parom parom = new Parom(20);
+            parom.subset = null;
+            parom.GreedyAlgorithm();
+            Assert.Equal(null, parom.subset);
+        }
 
+        [Fact]
+        public void GreedyAlgorithm_Alone()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(2, 18));
+            parom.GreedyAlgorithm();
+            Assert.Equal(parom.result[0].weight, parom.subset[0].weight);
+            Assert.Equal(parom.result[0].price, parom.subset[0].price);
+            Assert.Equal(parom.result[0].koef, parom.subset[0].koef);
+        }
+
+        [Fact]
+        public void GreedyAlgorithm_FULL()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(2, 18));
+            parom.subset.Add(new Automobile(10, 23));
+            parom.subset.Add(new Automobile(4, 211));
+            parom.subset.Add(new Automobile(4, 98));
+            parom.subset.Add(new Automobile(5, 28));
+            parom.GreedyAlgorithm();
+            Assert.Equal(parom.bestPrice, 355);
+        }
+
+        [Fact]
+        public void GreedyAlgorithm_Test()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(3, 67));
+            parom.subset.Add(new Automobile(2, 94));
+            parom.GreedyAlgorithm();
+            Assert.Equal(parom.subset[0].weight, parom.result[0].weight);
+            Assert.Equal(parom.subset[0].price, parom.result[0].price);
+            Assert.Equal(parom.subset[1].weight, parom.result[1].weight);
+            Assert.Equal(parom.subset[1].price, parom.result[1].price);
+        }
+
+        [Fact]
+        public void GreedyAlgorithm_SortKoef()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(3, 67));
+            parom.subset.Add(new Automobile(2, 94));
+            parom.GreedyAlgorithm();
+            Assert.Equal(parom.subset[0].weight, 2);
+            Assert.Equal(parom.subset[0].price, 94);
+            Assert.Equal(parom.subset[1].weight, 3);
+            Assert.Equal(parom.subset[1].price, 67);
         }
     }
+
+    public class EnumAlgoritm
+    {
+        [Fact]
+        public void EnumAuto_Empty()
+        {
+
+            Parom parom = new Parom(20);
+            parom.subset = null;
+            parom.EnumerationAutomobile(parom.subset);
+            Assert.Equal(null , parom.subset);
+        }
+
+        [Fact]
+        public void EnumAuto_FULL()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(2, 18));
+            parom.subset.Add(new Automobile(10, 23));
+            parom.subset.Add(new Automobile(4, 211));
+            parom.subset.Add(new Automobile(4, 98));
+            parom.subset.Add(new Automobile(5, 28));
+            parom.GreedyAlgorithm();
+            Assert.Equal(parom.bestPrice, 355);
+        }
+
+        [Fact]
+        public void EnumAuto_Alone()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(2, 18));
+            parom.EnumerationAutomobile(parom.subset);
+            Assert.Equal(parom.result[0].weight, parom.subset[0].weight);
+            Assert.Equal(parom.result[0].price, parom.subset[0].price);
+            Assert.Equal(parom.result[0].koef, parom.subset[0].koef);
+        }
+
+        [Fact]
+        public void EnumAuto_Test()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(3, 67));
+            parom.subset.Add(new Automobile(2, 94));
+            parom.EnumerationAutomobile(parom.subset);
+            Assert.Equal(parom.subset[0].weight, parom.result[0].weight);
+            Assert.Equal(parom.subset[0].price, parom.result[0].price);
+            Assert.Equal(parom.subset[1].weight, parom.result[1].weight);
+            Assert.Equal(parom.subset[1].price, parom.result[1].price);
+        }
+
+        [Fact]
+        public void CheckSet_Test()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(3, 67));
+            parom.subset.Add(new Automobile(2, 94));
+            parom.CheckSet(parom.subset);
+            Assert.Equal(parom.subset[0].weight, parom.result[0].weight);
+            Assert.Equal(parom.subset[0].price, parom.result[0].price);
+            Assert.Equal(parom.subset[1].weight, parom.result[1].weight);
+            Assert.Equal(parom.subset[1].price, parom.result[1].price);
+        }
+
+        [Fact]
+        public void Summ_Test()
+        {
+            Parom parom = new Parom(20);
+            parom.subset.Add(new Automobile(3, 67));
+            parom.subset.Add(new Automobile(2, 94));
+            Assert.Equal(parom.SummW(parom.subset), 5);
+            Assert.Equal(parom.SummP(parom.subset), 161);
+        }
+
+        [Fact]
+        public void Summ_TestEmpty()
+        {
+            Parom parom = new Parom(20);
+            parom.subset = null;
+            Assert.Equal(parom.SummW(parom.subset), 0);
+            Assert.Equal(parom.SummP(parom.subset), 0);
+        }
+    }
+
 }
